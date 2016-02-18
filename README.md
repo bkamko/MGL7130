44827731f653cb66c6524ffe83cef8264fef96e5
Warlot-PQ <warlot.pierre.quentin@gmail.com>
2016-02-17 23:18:33 -0500

    Interface d'accueil basique + navigation bouton filtre + rectification tutoriel README.md

--- README.md
+++ README.md
@@ -3,13 +3,21 @@
 ## Configuration de l'environnement de travail avec Ionic, Git et Github
 
 Placez-vous dans un repertoire de travail puis entrez les commandes suivantes :
+
 $ ionic start Simplyk blank
+
 A la question de la création d'un compte ionic.io répondez non. Vous pouvez le faire plus tard si vous voulez.
+
 $ cd Simplyk
+
 $ cd www
+
 $ git init .
+
 $ git remote add -t \* -f origin https://github.com/Warlot-PQ/MGL7130.git
+
 $ rm -Rf css/ img/ js/ index.html
+
 $ git checkout master
 
 L'environnement ionic est prêt et le lien avec GitHub est établi.
@@ -18,17 +26,23 @@ L'environnement ionic est prêt et le lien avec GitHub est établi.
 
 Placez-vous dans le repertoire Simplyk.
 
-Ajouter un watcher au fichier ionic.project pour que le live reload soit actif sur d'autres fichiers que sur index.html.
+Ajouter un watcher au fichier ionic.project pour que le live reload soit actif sur d'autres fichiers que sur index.html et une tache GULP pour activer le live reload.
 
 Exemple ionic.project :
+```
 {
   "name": "Simplyk",
   "app_id": "",
   "watchPatterns": [
     "www/js/*",
     "!www/css/**/*"
+  ],
+  "gulpStartupTasks": [
+    "sass",
+    "watch"
   ]
 }
+```
 
 Lancer le serveur simulant les appareils android et IOS :
 
@@ -36,5 +50,14 @@ $ ionic serve --lab
 
 ## Déploiement sur téléphone par l'application Ionic View
 
+Identifiez-vous avec votre login et mot de passe Ionic.
+
 $ ionic login
+
+Envoyez le code du projet sur internet.
+
 $ ionic upload
+
+Utilisez l'application mobile Ionic View pour visualier l'application Ionic.
+
+
--- js/index.js
+++ js/index.js
@@ -2,41 +2,18 @@ var angularApp = angular.module('MenuNav', ['ionic']);
 var angularScope;
 
 angularApp.config(function($stateProvider, $urlRouterProvider) {
-	$stateProvider.state('app', {
-		abstract: true,
-		url: '/app',
-		templateUrl: "menu.html",
-		controller: "AppCtrl"
-	})
-
-	.state('app.home', {
+	$stateProvider.state('home', {
 		url: '/home',
-		views: {
-			'sideNenuContent' :{
-				templateUrl: "home.html"
-			}
-		}
+		templateUrl: 'home.html',
+		controller: 'AppCtrl'
 	})
 
-  .state('app.other', {
-  	url: '/other',
-  	views: {
-  		'sideNenuContent' :{
-  			templateUrl: "other.html"
-  		}
-  	}
-  })
+	.state('filter', {
+		url: '/filter',
+		templateUrl: 'filter.html'
+	});
 
-  .state('app.filter', {
-  	url: '/filter',
-  	views: {
-  		'sideNenuContent' :{
-  			templateUrl: "filter.html"
-  		}
-  	}
-  });
-
-  $urlRouterProvider.otherwise('/app/home');
+	$urlRouterProvider.otherwise('/home');
 })
 
 angularApp.controller("AppCtrl", function($scope){
@@ -45,72 +22,66 @@ angularApp.controller("AppCtrl", function($scope){
 	angularScope.navigation = {
 		page1: {
 			title: 'Volunteer Seeker Map',
-			direction: "#/app/home"
-		},
-		page2: {
-			title: 'Volunteer Seeker List',
-			direction: "#/app/other"
-		},
-		page3: {
-			title: 'My Events',
-			direction: "#"
+			direction: "/home"
 		},
 		pageHeaderRight: {
 			title: 'Filter',
-			direction: "app.filter"
+			direction: "filter"
 		}
 	};
+
+	function initialize() {
+		var mapOptions = {
+			zoom: 10,
+			center: new google.maps.LatLng(-33.89, 151.27),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+		var markers = [
+			['Bondi Beach, 01/30/16 2:00 PM', -33.890542, 151.274856, 4],
+			['Coogee Beach, 01/30/16 2:00 PM', -33.923036, 151.259052, 5],
+			['Cronulla Beach, 01/30/16 2:00 PM', -34.028249, 151.157507, 3],
+			['Manly Beach, 01/30/16 2:00 PM', -33.80010128657071, 151.28747820854187, 2],
+			['Maroubra Beach, 01/30/16 2:00 PM', -33.950198, 151.259302, 1]
+		];
+
+		// Info windows displayed above each markers
+		var infowindow = new google.maps.InfoWindow();
+
+		// Loop through the array of markers and place each one on the map 
+		for(i = 0; i < markers.length; i += 1) {
+			marker = new google.maps.Marker({
+				position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+				map: map
+			});
+
+			// Add click action on each marcker
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			  	return function() {
+			  		infowindow.setContent(markers[i][0]);
+			  		infowindow.open(map, marker);
+
+			      // Display event informations
+			      eventInfoContent = markers[i][1] + " - " + markers[i][2];
+			      angularScope.$apply(function() {
+			      	angularScope.eventSelected = { 
+			      		name: markers[i][0],
+			      		desc: eventInfoContent + 
+			      		'<br />' + 
+			      		eventInfoContent + 
+			      		'<br />' + 
+			      		eventInfoContent
+			      	};
+			      });
+				}
+			})(marker, i));
+		}
+	}
+	google.maps.event.addDomListener(window, "load", initialize);
 });
 
-function initialize() {
-	var mapOptions = {
-		zoom: 10,
-		center: new google.maps.LatLng(-33.89, 151.27),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-
-	var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-	var markers = [
-		['Bondi Beach, 01/30/16 2:00 PM', -33.890542, 151.274856, 4],
-		['Coogee Beach, 01/30/16 2:00 PM', -33.923036, 151.259052, 5],
-		['Cronulla Beach, 01/30/16 2:00 PM', -34.028249, 151.157507, 3],
-		['Manly Beach, 01/30/16 2:00 PM', -33.80010128657071, 151.28747820854187, 2],
-		['Maroubra Beach, 01/30/16 2:00 PM', -33.950198, 151.259302, 1]
-	];
-
-	// Info windows displayed above each markers
-	var infowindow = new google.maps.InfoWindow();
-
-	// Loop through the array of markers and place each one on the map 
-	for(i = 0; i < markers.length; i += 1) {
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(markers[i][1], markers[i][2]),
-			map: map
-		});
-
-		// Add click action on each marcker
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		  	return function() {
-		  		infowindow.setContent(markers[i][0]);
-		  		infowindow.open(map, marker);
-
-		      // Display event informations
-		      eventInfoContent = markers[i][1] + " - " + markers[i][2];
-		      angularScope.$apply(function() {
-		      	angularScope.eventSelected = { 
-		      		name: markers[i][0],
-		      		desc: eventInfoContent + 
-		      		'<br />' + 
-		      		eventInfoContent + 
-		      		'<br />' + 
-		      		eventInfoContent
-		      	};
-		      });
-			}
-		})(marker, i));
-	}
-}
 
 var app = {
 	initialize: function() {
